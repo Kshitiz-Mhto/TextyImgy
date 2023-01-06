abd17c7367092a1e1bb72ba5a254e0ff55bb2f28
safarx <ibergx00@gmail.com>
2023-01-07 01:55:56 +0545

    play with ai hehehe

---
+++ ImgGen/apiAi.py
@@ -0,0 +1,47 @@
+import os
+import io
+import random
+import warnings
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+
+# Our Host URL should not be prepended with "https" nor should it have a trailing slash.
+# os.environ['STABILITY_HOST'] = 'grpc.stability.ai:443'
+
+# Paste your API Key below.
+# os.environ['STABILITY_KEY'] = 'key-goes-here'
+
+# Set up our connection to the API.
+stability_api = client.StabilityInference(
+    key=os.environ['STABILITY_KEY'], # API Key reference.
+    verbose=True, # Print debug messages.
+    engine="stable-diffusion-v1-5",
+)
+
+number = random.randint(0, 2147483647)
+data = str(input("Enter description of your Image: "))
+# Set up our initial generation parameters.
+answers = stability_api.generate(
+    prompt=data,
+    seed=number, 
+    steps=30, 
+    cfg_scale=8.0, 
+    width=512, # Generation width, defaults to 512 if not included.
+    height=512, # Generation height, defaults to 512 if not included.
+    samples=3, # Number of images to generate, defaults to 1 if not included.
+    sampler=generation.SAMPLER_K_DPMPP_2M 
+                                        
+)
+
+for resp in answers:
+    for artifact in resp.artifacts:
+        if artifact.finish_reason == generation.FILTER:
+            warnings.warn(
+                "Your request activated the API's safety filters and could not be processed."
+                "Please modify the prompt and try again.")
+            break
+        if artifact.type == generation.ARTIFACT_IMAGE:
+            img = Image.open(io.BytesIO(artifact.binary))
+            img.save("./assets/"+str(artifact.seed)+ ".png") # Save our generated images with their seed number as the filename.
+
